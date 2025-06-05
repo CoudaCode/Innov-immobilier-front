b2c093660e0a65650054c3326cf3aae820a51329
Diarassouba Mamadou <couda.dm@gmail.com>
2025-06-05 21:08:31 +0000

    update

--- js/index.js
+++ js/index.js
@@ -19,44 +19,5 @@ document.addEventListener("DOMContentLoaded", async () => {
   );
   console.log(images);
 
-  slider.innerHTML = images.map((image, index) => {
-    return `<div class="item" data-index="${index}" key="${index}">
-                    <div
-                      class="hover overflow-hidden relative text-light text-center"
-                    >
-                      <div class="overflow-hidden">
-                        <img
-                          src=${image}
-                          class="hover-scale-1-1 w-100"
-                          alt=""
-                        />
-                      </div>
-                      <div
-                        class="abs w-100 px-4 hover-op-1 z-4 hover-mt-40 abs-centered"
-                      >
-                        <a
-                          class="btn-main btn-line fx-slide"
-                          href="https://africabooking.app/"
-                          ><span> Details</span></a
-                        >
-                      </div>
-                      <div
-                        class="abs bg-blur z-2 top-0 w-100 h-100 hover-op-1"
-                      ></div>
-                      <div
-                        class="abs z-2 bottom-0 p-30 w-100 text-center hover-op-0"
-                      >
-                        <div
-                          class="d-flex justify-content-between align-items-center"
-                        >
-                          <!--  <h3 class="mb-0">KIA ORA VERT</h3> -->
-                          <div></div>
-                        </div>
-                      </div>
-                      <div
-                        class="gradient-edge-bottom abs w-100 h-40 bottom-0"
-                      ></div>
-                    </div>
-                  </div>`;
-  });
+  slider.innerHTML = images.map((image, index) => {});
 });
